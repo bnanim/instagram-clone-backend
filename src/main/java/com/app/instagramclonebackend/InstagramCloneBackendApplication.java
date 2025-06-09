b9306552b85566c2c37cb4462f834112d614fbc7
bnanim <bernardanim86@gmail.com>
2025-06-09 13:42:20 +0000

    Initialize Spring Boot project with Maven, basic configuration, and default setup.

---
+++ src/main/java/com/app/instagramclonebackend/InstagramCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.instagramclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramCloneBackendApplication.class, args);
+    }
+
+}
